315043685c46a40186777516bdd4f912b6a82d32
16cheng <474119@qq.com>
2019-07-14 21:43:45 +0800

    微信小程序（原生）新增K线图示例

--- 示例项目/微信小程序（原生）/pages/index/index.js
+++ 示例项目/微信小程序（原生）/pages/index/index.js
@@ -2,6 +2,7 @@ import uCharts from '../ucharts/u-charts.js';
 var _self;
 var canvaColumn = null;
 var canvaLineA = null;
+var canvaCandle = null;
 Page({
   data: {
     cWidth: '',
@@ -30,8 +31,13 @@ Page({
         //这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
         LineA.categories = res.data.data.LineA.categories;
         LineA.series = res.data.data.LineA.series;
+        let Candle = {categories: [],series: []};
+        //这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+        Candle.categories = res.data.data.Candle.categories;
+        Candle.series = res.data.data.Candle.series;
         _self.showColumn("canvasColumn", Column);
         _self.showLineA("canvasLineA", LineA);
+        _self.showCandle("canvasCandle", Candle);
       },
       fail: () => {
         console.log("请点击右上角【详情】，启用不校验合法域名");
@@ -132,6 +138,87 @@ Page({
     canvaLineA.scrollEnd(e);
     //下面是toolTip事件，如果滚动后不需要显示，可不填写
     canvaLineA.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  showCandle(canvasId, chartData) {
+    canvaCandle = new uCharts({
+      $this: _self,
+      canvasId: canvasId,
+      type: 'candle',
+      fontSize: 11,
+      legend: true,
+      background: '#FFFFFF',
+      pixelRatio: 1,
+      categories: chartData.categories,
+      series: chartData.series,
+      animation: true,
+      enableScroll: true,
+      xAxis: {
+        disableGrid: true,
+        itemCount: 20,
+        scrollShow: true,
+        scrollAlign: 'right',
+        labelCount:4,
+      },
+      yAxis: {
+        //disabled:true
+        gridType: 'dash',
+        splitNumber: 5,
+        format: (val) => {
+          return val.toFixed(0)
+        }
+      },
+      width: _self.cWidth,
+      height: _self.cHeight,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        candle: {
+          color: {
+            upLine: '#f04864',
+            upFill: '#f04864',
+            downLine: '#2fc25b',
+            downFill: '#2fc25b'
+          },
+          average: {
+            show: true,
+            name: ['MA5', 'MA10', 'MA30'],
+            day: [5, 10, 30],
+            color: ['#1890ff', '#2fc25b', '#facc14']
+          }
+        },
+        tooltip: {
+          bgColor: '#000000',
+          bgOpacity: 0.7,
+          gridType: 'dash',
+          dashLength: 5,
+          gridColor: '#1890ff',
+          fontColor: '#FFFFFF',
+          horizentalLine: true,
+          xAxisLabel: true,
+          yAxisLabel: true,
+          labelBgColor: '#DFE8FF',
+          labelBgOpacity: 0.95,
+          labelAlign: 'left',
+          labelFontColor: '#666666'
+        }
+      },
+    });
+
+  },
+  touchCandle(e) {
+    canvaCandle.scrollStart(e);
+  },
+  moveCandle(e) {
+    canvaCandle.scroll(e);
+  },
+  touchEndCandle(e) {
+    canvaCandle.scrollEnd(e);
+    //下面是toolTip事件，如果滚动后不需要显示，可不填写
+    canvaCandle.showToolTip(e, {
       format: function (item, category) {
         return category + ' ' + item.name + ':' + item.data
       }
