cdc487ce95dafe7f7c250f923899c34aa72b04cd
16cheng <474119@qq.com>
2019-07-14 09:16:51 +0800

    原生微信小程序新增拖拽示例

--- 示例项目/微信小程序（原生）/pages/index/index.js
+++ 示例项目/微信小程序（原生）/pages/index/index.js
@@ -1,6 +1,7 @@
 import uCharts from '../ucharts/u-charts.js';
 var _self;
 var canvaColumn = null;
+var canvaLineA = null;
 Page({
   data: {
     cWidth: '',
@@ -25,7 +26,12 @@ Page({
         //自定义标签颜色和字体大小
         Column.series[1].textColor = 'red';
         Column.series[1].textSize = 18;
+        let LineA = { categories: [], series: [] };
+        //这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+        LineA.categories = res.data.data.LineA.categories;
+        LineA.series = res.data.data.LineA.series;
         _self.showColumn("canvasColumn", Column);
+        _self.showLineA("canvasLineA", LineA);
       },
       fail: () => {
         console.log("请点击右上角【详情】，启用不校验合法域名");
@@ -72,5 +78,63 @@ Page({
         }
       }
     });
-  }
+  },
+  showLineA(canvasId, chartData) {
+    canvaLineA = new uCharts({
+      $this: _self,
+      canvasId: canvasId,
+      type: 'line',
+      fontSize: 11,
+      legend: true,
+      dataLabel: true,
+      dataPointShape: true,
+      background: '#FFFFFF',
+      pixelRatio: 1,
+      categories: chartData.categories,
+      series: chartData.series,
+      animation: true,
+      enableScroll: true,//开启图表拖拽功能
+      xAxis: {
+        disableGrid: false,
+        type: 'grid',
+        gridType: 'dash',
+        itemCount: 4,
+        scrollShow: true,
+        scrollAlign: 'left',
+        //scrollBackgroundColor:'#F7F7FF',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条背景颜色,默认为 #EFEBEF
+        //scrollColor:'#DEE7F7',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条颜色,默认为 #A6A6A6
+      },
+      yAxis: {
+        //disabled:true
+        gridType: 'dash',
+        splitNumber: 8,
+        min: 10,
+        max: 180,
+        format: (val) => { return val.toFixed(0) + '元' }//如不写此方法，Y轴刻度默认保留两位小数
+      },
+      width: _self.cWidth,
+      height: _self.cHeight,
+      extra: {
+        line: {
+          type: 'straight'
+        }
+      },
+    });
+
+  },
+  touchLineA(e) {
+    canvaLineA.scrollStart(e);
+  },
+  moveLineA(e) {
+    canvaLineA.scroll(e);
+  },
+  touchEndLineA(e) {
+    canvaLineA.scrollEnd(e);
+    //下面是toolTip事件，如果滚动后不需要显示，可不填写
+    canvaLineA.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
 })
