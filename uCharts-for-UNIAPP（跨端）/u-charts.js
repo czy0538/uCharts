31a0239d69735eb9a586b023509afb1266860c56
不是发哥 <zz.f@foxmail.com>
2019-07-30 12:19:48 +0800

    修改config为深拷贝、解决引用类型问题

--- uCharts-for-UNIAPP（跨端）/u-charts.js
+++ uCharts-for-UNIAPP（跨端）/u-charts.js
@@ -1,5 +1,5 @@
 /*
- * uCharts v1.8.1.20190727
+ * uCharts v1.8.1.20190730
  * uni-app平台高性能跨全端图表，支持H5、APP、小程序（微信/支付宝/百度/头条/QQ/360）
  * Copyright (c) 2019 QIUN秋云 https://www.ucharts.cn All rights reserved.
  * Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
@@ -3943,7 +3943,7 @@ var Charts = function Charts(opts) {
   opts.rotate = opts.rotate ? true : false;
   opts.animation = opts.animation ? true : false;
 
-  let config$$1 = assign({}, config);
+  let config$$1 = JSON.parse(JSON.stringify(config));
   config$$1.colors = opts.colors ? opts.colors : config$$1.colors;
   config$$1.yAxisTitleWidth = opts.yAxis.disabled !== true && opts.yAxis.title ? config$$1.yAxisTitleWidth : 0;
   if (opts.type == 'pie' || opts.type == 'ring') {
