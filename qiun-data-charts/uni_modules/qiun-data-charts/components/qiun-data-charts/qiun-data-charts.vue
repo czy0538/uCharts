b854a737800c9fee8a500d0ad53efff8692e3a38
16cheng <474119@qq.com>
2021-04-14 23:14:51 +0800

    修复H5端ECharts在cli项目下引用地址错误的bug

--- qiun-data-charts/uni_modules/qiun-data-charts/components/qiun-data-charts/qiun-data-charts.vue
+++ qiun-data-charts/uni_modules/qiun-data-charts/components/qiun-data-charts/qiun-data-charts.vue
@@ -1,5 +1,5 @@
 <!-- 
- * qiun-data-charts 秋云高性能跨全端图表组件 v2.0.0-20210413
+ * qiun-data-charts 秋云高性能跨全端图表组件 v2.0.0-20210414
  * Copyright (c) 2021 QIUN® 秋云 https://www.ucharts.cn All rights reserved.
  * Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
  * 复制使用请保留本段注释，感谢支持开源！
@@ -1157,7 +1157,7 @@ export default {
         script.src = './uni_modules/qiun-data-charts/static/app-plus/echarts.min.js'
         // #endif
         // #ifdef H5
-        script.src = '/uni_modules/qiun-data-charts/static/h5/echarts.min.js'
+        script.src = './uni_modules/qiun-data-charts/static/h5/echarts.min.js'
         // #endif
         script.onload = this.newEChart
         document.head.appendChild(script)
