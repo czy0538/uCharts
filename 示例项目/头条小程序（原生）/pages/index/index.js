5d605e649319be87a8176ab0f53187060f942ed7
16cheng <474119@qq.com>
2021-12-09 23:05:02 +0800

    变更目录，修改u-charts.js为v2.3.6-20211201，修改调动方式

--- 示例项目/头条小程序（原生）/pages/index/index.js
+++ 示例项目/头条小程序（原生）/pages/index/index.js
@@ -1,76 +1,75 @@
-import uCharts from '../ucharts/u-charts.js';
-var _self;
-var canvaColumn = null;
+//index.js
+import uCharts from '../../js_sdk/u-charts/u-charts.js';
+var uChartsInstance = {};
 Page({
   data: {
-    cWidth: '',
-    cHeight: '',
+    cWidth: 750,
+    cHeight: 500
   },
-  onLoad: function () {
-    _self=this;
-    this.cWidth = tt.getSystemInfoSync().windowWidth;
-    this.cHeight = 500 / 750 * tt.getSystemInfoSync().windowWidth;
+  onReady() {
+    //这里的第一个 750 对应 css .charts 的 width
+    const cWidth = 750 / 750 * tt.getSystemInfoSync().windowWidth;
+    //这里的 500 对应 css .charts 的 height
+    const cHeight = 500 / 750 * tt.getSystemInfoSync().windowWidth;
+    this.setData({ cWidth, cHeight });
     this.getServerData();
   },
-  getServerData: function() {
-    tt.request({
-      url: 'https://www.ucharts.cn/data.json',
-      data: {
-      },
-      success: function (res) {
-        console.log(res.data.data)
-        let Column = { categories: [], series: [] };
-        Column.categories = res.data.data.ColumnB.categories;
-        Column.series = res.data.data.ColumnB.series;
-        //自定义标签颜色和字体大小
-        Column.series[1].textColor = 'red';
-        Column.series[1].textSize = 18;
-        _self.showColumn("canvasColumn", Column);
-      },
-      fail: () => {
-        console.log("请点击右上角【详情】，启用不校验合法域名");
-      },
-    });
+  getServerData() {
+    //模拟从服务器获取数据时的延时
+    setTimeout(() => {
+      //模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
+      let res = {
+            categories: ["2016","2017","2018","2019","2020","2021"],
+            series: [
+              {
+                name: "目标值",
+                data: [35,36,31,33,13,34]
+              },
+              {
+                name: "完成量",
+                data: [18,27,21,24,6,28]
+              }
+            ]
+          };
+      this.drawCharts('afMCYQMEmXXVAjNQFJvvfxbLSHuxNEOL', res);
+    }, 500);
   },
-  showColumn(canvasId, chartData) {
-    let ctx = tt.createCanvasContext(canvasId, this);
-    canvaColumn = new uCharts({
-      type: 'column',
-      context: ctx,
-      legend: true,
-      fontSize: 11,
-      background: '#FFFFFF',
-      pixelRatio: 1,
-      animation: true,
-      categories: chartData.categories,
-      series: chartData.series,
-      xAxis: {
-        disableGrid: true,
-      },
-      yAxis: {
-        //disabled:true
-      },
-      dataLabel: true,
-      width: _self.cWidth ,
-      height: _self.cHeight ,
-      extra: {
-        column: {
-          type: 'group',
-          width: _self.cWidth * 0.45 / chartData.categories.length
+  drawCharts(id,data){
+    const ctx = tt.createCanvasContext(id, this);
+    uChartsInstance[id] = new uCharts({
+        animation: true,
+        background: "#FFFFFF",
+        categories: data.categories,
+        color: ["#1890FF","#91CB74","#FAC858","#EE6666","#73C0DE","#3CA272","#FC8452","#9A60B4","#ea7ccc"],
+        context: ctx,
+        extra: {
+          column: {
+            type: "group",
+            width: 30,
+            activeBgColor: "#000000",
+            activeBgOpacity: 0.08
+          }
+        },
+        height: this.data.cHeight,
+        legend: {},
+        padding: [15,15,0,5],
+        series: data.series,
+        type: "column",
+        width: this.data.cWidth,
+        xAxis: {
+          disableGrid: true
+        },
+        yAxis: {
+          data: [
+            {
+              min: 0
+            }
+          ]
         }
-      }
-    });
-
+      });
   },
-  touchColumn(e) {
-    canvaColumn.showToolTip(e, {
-      format: function (item, category) {
-        if (typeof item.data === 'object') {
-          return category + ' ' + item.name + ':' + item.data.value
-        } else {
-          return category + ' ' + item.name + ':' + item.data
-        }
-      }
-    });
+  tap(e){
+    uChartsInstance[e.target.id].touchLegend(e);
+    uChartsInstance[e.target.id].showToolTip(e);
   }
 })
